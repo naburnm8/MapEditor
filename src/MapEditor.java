588ad241aab2d7891234f6335fb786ab9b41a32f
naburnm8 <alint@bk.ru>
2024-04-15 17:57:56 +0300

    Main menu

--- src/MapEditor.java
+++ src/MapEditor.java
@@ -5,40 +5,16 @@ import java.util.Scanner;
 
 public class MapEditor {
     public static void main(String[] args) throws IOException, IncorrectFileStructure {
-        /*
-        Field test2 = new Field(10,10);
-        FileWriter fw = new FileWriter("file.txt");
-        fw.write("10\n");
-        fw.write("10\n");
-        fw.write(test2.toString());
-        System.out.println(test2);
-        fw.close();
+            System.out.println("Welcome to Bauman's Gate map editor.\nSyntax: -load path -create name");
+            Scanner stream = new Scanner(System.in);
+            while(true){
+                String read = stream.nextLine();
+                String[] split = read.split(" ");
+                if (split[0].equals("-load") || split[0].equals("-create")){
+                    break;
+                }
+                System.out.println("Wrong syntax!");
+            }
 
-        FileReader fr = new FileReader("file.txt");
-        Scanner stream = new Scanner(fr);
-        stream.useDelimiter("\\Z");
-        System.out.println(stream.next());
-        fr.close();
-        FileReader fr = new FileReader("file.txt");
-        Scanner stream = new Scanner(fr);
-        System.out.println(stream.nextInt());
-        System.out.println(stream.nextInt());
-        stream.nextLine();
-        stream.useDelimiter("\\Z");
-        System.out.println(stream.next());
-        Field test1 = new Field(10,10);
-
-        MapReader reader = new MapReader("file.txt");
-        Field test1 = new Field(reader.read());
-        System.out.println(test1);
-        */
-        MapData test = new MapData(6,6);
-        MapWriter writer = new MapWriter("test1.txt");
-        writer.write(test);
-        MapReader reader = new MapReader("file.txt");
-        MapData test2 = reader.read();
-        System.out.println(test2);
-        System.out.println(test2.width);
-        System.out.println(test2.height);
         }
     }
